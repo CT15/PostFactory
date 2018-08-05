f1088e9c45ae27dc8f5c158815dbe92420216592
Calvin Tantio <calvin.tantio@outlook.com>
2018-08-05 14:17:49 +0800

    Fix url string

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -34,7 +34,7 @@ export class AuthService {
     this.loadToken();
     headers.append('Authorization', this.authToken)
     headers.append('Content-Type', 'application/json');
-    return this.http.get('http://localhost3000/users/profile', { headers: headers })
+    return this.http.get('http://localhost:3000/users/profile', { headers: headers })
                 .pipe(map(res => res.json()));
   }
 
